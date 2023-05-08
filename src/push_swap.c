5f9781dc306c5dde66059d8bbbd51af6f31f711e
flolau2k <florian.lauer@live.de>
2023-05-08 13:50:59 +0200

    fix algo

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 11:55:50 by flauer            #+#    #+#             */
-/*   Updated: 2023/05/08 13:15:42 by flauer           ###   ########.fr       */
+/*   Updated: 2023/05/08 13:42:01 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -200,10 +200,8 @@ int	get_chunk_pos(t_state *st, t_list *elm)
 	tmp = st->b;
 	i = 0;
 	len = ft_lstsize(tmp);
-	while (tmp)
+	while (tmp && same_chunk(st, tmp, elm))
 	{
-		if (same_chunk(st, tmp, elm) == 0)
-			break ;
 		tmp = tmp->next;
 		++i;
 	}
@@ -220,6 +218,19 @@ int	get_chunk_pos(t_state *st, t_list *elm)
 	}
 }
 
+int	get_steps_combined(int nra, int nrb)
+{
+	if ((nra >= 0 && nrb >= 0) || (nra < 0 && nrb < 0))
+	{
+		if (ft_abs(nra) > ft_abs(nrb))
+			return(ft_abs(nra));
+		else
+			return(ft_abs(nrb));
+	}
+	else
+		return(ft_abs(nra) + ft_abs(nrb));
+}
+
 /// @brief set nsteps for all elements in stack a. TODO: ft_lstiter()?
 /// @param st state
 void	get_pb_steps(t_state *st)
@@ -227,17 +238,17 @@ void	get_pb_steps(t_state *st)
 	t_list	*curr_elm;
 	int		nra;
 	int		nrb;
+	int		nsteps;
 
 	curr_elm = st->a;
 	while (curr_elm)
 	{
 		nrb = get_chunk_pos(st, curr_elm);
 		nra = get_pos(st->a, curr_elm);
-		if ((nra >= 0 && nrb >= 0) || (nra < 0 && nrb < 0))
-			nra = ft_abs(ft_abs(nra) - ft_abs(nrb));
+		nsteps = get_steps_combined(nra, nrb);
 		if (get_id(curr_elm) == 0 || get_id(curr_elm) == st->len - 1)
-			nra = 999999;
-		((t_elm *)curr_elm->content)->nsteps = nra;
+			nsteps = INT32_MAX;
+		((t_elm *)curr_elm->content)->nsteps = nsteps;
 		curr_elm = curr_elm->next;
 	}
 }
@@ -249,15 +260,15 @@ void	get_pa_steps(t_state *st)
 	t_list	*curr_elm;
 	int		nra;
 	int		nrb;
+	int		nsteps;
 
 	curr_elm = st->b;
 	while (curr_elm)
 	{
 		nrb = get_pos(st->b, curr_elm);
 		nra = _ft_ins(st->a, curr_elm);
-		if ((nra >= 0 && nrb >= 0) || (nra < 0 && nrb < 0))
-			nra = ft_abs(ft_abs(nra) - ft_abs(nrb));
-		((t_elm *)curr_elm->content)->nsteps = nra;
+		nsteps = get_steps_combined(nra, nrb);
+		((t_elm *)curr_elm->content)->nsteps = nsteps;
 		curr_elm = curr_elm->next;
 	}
 }
